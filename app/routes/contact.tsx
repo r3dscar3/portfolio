edb8ab36b6e61a2a5897fe5a58803f228af3c1a8
Nolan Thompson <nolan.thompson@getfliff.com>
2024-11-18 11:28:34 -0700

    Email with resend

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -22,8 +22,8 @@ export async function action({ request }: ActionFunctionArgs) {
     html: `
       <div>
         <h1>Form submission from nolanpanther.com</h1>
-        <h2> style='padding: 4px 0'>${subject}</h2>
-        <p style='padding: 4px 0'>${body}</p>
+        <h2 style="padding: 4px 0">${subject}</h2>
+        <p style="padding: 4px 0">${body}</p>
       </div>
     `,
   });
@@ -57,7 +57,7 @@ export default function Contact() {
       {error ? (
         <div className="text-red-700 bg-red-100 border border-red-300 rounded-md w-full max-w-[400px] mx-auto p-6">
           {error.message}
-        </div>
+        </div>  
       ) : formData ? (
         <div className="text-green-700 bg-green-100 border border-green-300 rounded-md w-full max-w-[400px] mx-auto p-6">
           Message sent successfully!
