d34bc52fd1bf0afa249365587c52812e8fac5a6a
Nolan Thompson <nolanpanther@yahoo.com>
2026-04-23 17:05:36 -0700

    Updating home page

--- app/components/PageWrapper.tsx
+++ app/components/PageWrapper.tsx
@@ -11,7 +11,7 @@ const PageWrapper = ({ children, heading, emoji }: PageWrapperProps) => {
     <div className="relative flex h-full flex-col overflow-y-auto">
       <div className="z-10 sticky top-0 flex items-center bg-white border-b border-gray-300 p-4 font-bold bg-opacity-65 backdrop-blur-sm">
         <h1>{heading}</h1>
-        <div className="w-6 h-6 ml-auto">{emoji}</div>
+        <div className="ml-auto text-3xl">{emoji}</div>
       </div>
 
       <div className="relative h-full p-4">
