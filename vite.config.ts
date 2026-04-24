d9e17e904a468aba1a6ef1ebd7ff46307bf11336
Nolan Thompson <nolanpanther@yahoo.com>
2026-04-24 10:59:45 -0700

    global updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
-import devtoolsJson from 'vite-plugin-devtools-json';
 import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), devtoolsJson()],
+  plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
   },
