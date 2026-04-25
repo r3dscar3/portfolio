99e6c93971c10301699ac7b43beac40ea302e8a1
Nolan Thompson <nolanpanther@yahoo.com>
2026-04-25 11:21:45 -0700

    cleanup

--- app/utils/validation.ts
+++ app/utils/validation.ts
@@ -1,11 +1,9 @@
-// prettier-ignore
-// eslint-disable-next-line
-const YUP_EMAIL_REGEXP =/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i;
-const NO_WHITESPACE_REGEXP = /\S+.*/;
-// eslint-disable-next-line
-const PHONE_REGEXP = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
-const UUID_REGEXP = /^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$/;
-const USERNAME_REGEXP = /^(?=[a-zA-Z0-9_\-–]{4,32}$)(?!.*[_\-–]{2})[^_\-–].*[^_\-–]$/;
+const YUP_EMAIL_REGEX =
+  /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i;
+const NO_WHITESPACE_REGEX = /\S+.*/;
+const PHONE_REGEX = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+const UUID_REGEX = /^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$/;
+const USERNAME_REGEX = /^(?=[a-zA-Z0-9_\-–]{4,32}$)(?!.*[_\-–]{2})[^_\-–].*[^_\-–]$/;
 
 const validation = {
   isRequired:
@@ -39,14 +37,14 @@ const validation = {
     (config: any = { message: 'Must use valid email address' }): any =>
     (value: string): string | null => {
       if (value === '') return null;
-      return !YUP_EMAIL_REGEXP.test(value) ? config.message : null;
+      return !YUP_EMAIL_REGEX.test(value) ? config.message : null;
     },
 
   isPhone:
     (config: any = { message: 'Must use valid phone number' }): any =>
     (value: string): string | null => {
       if (value === '') return null;
-      return !PHONE_REGEXP.test(value) ? config.message : null;
+      return !PHONE_REGEX.test(value) ? config.message : null;
     },
 
   isNumber:
@@ -68,7 +66,7 @@ const validation = {
     },
 
   isEmpty: (value: any) => {
-    return !NO_WHITESPACE_REGEXP.test(value);
+    return !NO_WHITESPACE_REGEX.test(value);
   },
 
   isPositiveNumber:
@@ -89,14 +87,14 @@ const validation = {
     (config: any): any =>
     (value: string): string | null => {
       if (value === '') return null;
-      return !UUID_REGEXP.test(value) ? config.message : null;
+      return !UUID_REGEX.test(value) ? config.message : null;
     },
 
   isUsername:
     (config: any): any =>
     (value: string): string | null => {
       if (value === '') return null;
-      return !USERNAME_REGEXP.test(value) ? config.message : null;
+      return !USERNAME_REGEX.test(value) ? config.message : null;
     },
 };
 
